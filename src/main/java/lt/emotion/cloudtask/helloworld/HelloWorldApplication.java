f84c10e13719a1da6ec539dcd6e253171b426528
Remigijus Bauzys <bauzys@gmail.com>
2020-08-11 00:19:59 +0300

    added mysql db support. introduced dev and prod config environment

--- src/main/java/lt/emotion/cloudtask/helloworld/HelloWorldApplication.java
+++ src/main/java/lt/emotion/cloudtask/helloworld/HelloWorldApplication.java
@@ -3,9 +3,11 @@ package lt.emotion.cloudtask.helloworld;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableTask
 public class HelloWorldApplication {
 
     @Bean
